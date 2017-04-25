cde6e4500cac9230238bdff3c10765be5ff7552a
Scott Philip <sp@scottphilip.com>
2017-04-25 10:34:47 +0200

    Initial commit

---
+++ FreePBX/Superfecta-Source.php
@@ -0,0 +1,107 @@
+<?php
+
+class CallerLookup extends superfecta_base{
+
+  public $description = "CallerLookup (TrueCaller)";
+  public $version_requirement = "2.11";
+  public $source_param = array(
+    'AccountInfo' => array(
+      'description' => "Google Account Credentials (Two factor authentication must be disabled):",
+      'type' => 'info'
+    ),
+    'Username' => array(
+      'description' => "Email",
+      'type' => 'text'
+    ),
+    'Password' => array(
+      'description' => "Password",
+      'type' => 'password'
+    ),
+    'Default_Region' => array(
+      'description' => "Default Country CCN",
+      'type' => 'text',
+      'default' => 'GB'
+    ),
+    'Spam_Threshold' => array(
+      'description' => "Minimum Score (0 - 10)",
+      'type' => 'number',
+      'default' => '1'
+    ),
+    'Add_Phonebook' => array(
+      'description' => "Add to Phonebook?",
+      'type' => 'checkbox',
+      'default' => 'true'
+    ),
+    'Add_Blacklist' => array(
+      'description' => "Add Spam Calls to Blacklist?",
+      'type' => 'checkbox',
+      'default' => 'false'
+    )
+  );
+
+  function get_caller_id($thenumber, $run_param=array()) {
+
+    $this->spam = false;
+    $is_debug = $this->debug;
+    $result_name = NULL;
+
+    $username = $run_param['Username'];
+    $password = $run_param['Password'];
+    $spam_threshold = isset($run_param['Spam_Threshold']) ? $run_param['Spam_Threshold'] : 0;
+    $is_add_phonebook = $run_param['Add_Phonebook'];
+    $is_add_blacklist = $run_param['Add_Blacklist'];
+    $default_region = $run_param['Default_Region'];
+
+    $this->DebugPrint(sprintf("CallerLookup: Username -> %s", $username));
+    $this->DebugPrint(sprintf("CallerLookup: Default Region -> %s", $default_region));
+    $this->DebugPrint(sprintf("CallerLookup: Threshold -> %s", $spam_threshold));
+    $this->DebugPrint(sprintf("CallerLookup: Add To Phonebook? -> %s", $is_add_phonebook?"Yes":"No"));
+    $this->DebugPrint(sprintf("CallerLookup: Add To Blacklist? -> %s", $is_add_blacklist?"Yes":"No"));
+
+    $script_path = '/var/lib/asterisk/agi-bin/CallerLookup.py'
+    $command = sprintf("python %s --number %s --region %s --username %s --password %s %s",
+                    $script_path,
+                    $thenumber,
+                    $default_region,
+                    $is_debug?"--debug":"")
+    $this->DebugPrint(sprintf("CallerLookup: Command -> %s", $command));
+
+    exec($command, $output, $return_var);
+    $this->DebugPrint(sprintf("CallerLookup: Result Code -> %s", $return_var));
+
+    if ($return_var == 0) {
+
+        $this->DebugPrint(sprintf("CallerLookup: Result Data -> %s", $output));
+        $result = json_decode($output, true);
+
+        if (isset($result['Result']) && $result['Result'] == "success") {
+
+            if (isset($result['Score']) && $result['Score'] >= 0) {
+                if (($result['Score'] * 10) < $spam_threshold) {
+                    $this->spam = true;
+                }
+            }
+
+            if (issset($result['Name']) && len($result['Name']) > 0) {
+                $result_name = $result['Name'];
+            }
+
+            if ($this->spam) {
+                if ($is_add_blacklist) {
+                    if (is_null($result_name)) {
+                        $result_name = "SPAM";
+                    }
+                    $this->DebugPrint("CallerLookup: Blacklisting - $result_name ($thenumber)");
+                    #TODO: Add Blacklisting
+                }
+            } else {
+                if ($is_add_phonebook && !is_null($result_name)) {
+                    #TODO: Add Phonebook
+                }
+            }
+        }
+    }
+
+	return is_null($result_name) ? "" : $result_name;
+  }
+}
