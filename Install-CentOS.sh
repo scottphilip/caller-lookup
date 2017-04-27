36be2f90469c9cc640749538f81212f02c697660
Scott Philip <sp@scottphilip.com>
2017-04-28 00:35:05 +0200

    Initial commit

--- Install-CentOS.sh
+++ Install-CentOS.sh
@@ -4,6 +4,7 @@ cd /tmp
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 NC='\033[0m'
 
 confirm() {
@@ -11,10 +12,10 @@ confirm() {
     read -r -p "${1:-Are you sure? [y/N]} " response
     case "$response" in
         [yY][eE][sS]|[yY])
-            true
+            false
             ;;
         *)
-            false
+            true
             ;;
     esac
 }
@@ -22,36 +23,46 @@ confirm() {
 #Check SUDO
 if [ "$EUID" -ne 0 ]
 then
-	printf "${RED}This must be run as root or with sudo.{NC}\n"
+	printf "${RED}This must be run as root or with sudo.${NC}\n"
     exit
 fi
 
-(!confirm "Are you sure you want to install CallerLookup and its dependencies? [y/N] ") && exit
+confirm "Are you sure you want to install CallerLookup? [y/N] " && exit
 
-printf "${GREEN}Updating Packages...{NC}\n"
+printf "${GREEN}Adding Source...${NC}\n"
+/usr/bin/yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+
+printf "${GREEN}Updating Packages...${NC}\n"
 /usr/bin/yum update
 
-printf "${GREEN}Python 3.6...{NC}\n"
-/usr/bin/yum install python36u
+printf "${GREEN}Python...${NC}\n"
+wget http://python.org/ftp/python/3.6.0/Python-3.6.0.tar.xz
+tar xf Python-3.6.0.tar.xz
+cd Python-3.6.0
+./configure --prefix=/usr/local --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib"
+make && make altinstall
 
-printf "${GREEN}Python PIP 3.6...{NC}\n"
-/usr/bin/yum install python36u-pip
+printf "${GREEN}Python PIP...${NC}\n"
+wget https://bootstrap.pypa.io/get-pip.py
+python3.6 get-pip.py
 
-printf "${GREEN}Selenium...{NC}\n"
+printf "${GREEN}Selenium...${NC}\n"
 /usr/bin/pip install selenium
 
-printf "${GREEN}PhoneNumbers...{NC}\n"
+printf "${GREEN}PhoneNumbers...${NC}\n"
 /usr/bin/pip install phonenumbers
 
-printf "${GREEN}Pyotp...{NC}\n"
+printf "${GREEN}Pyotp...${NC}\n"
 /usr/bin/pip install pyotp
 
-printf "${GREEN}Saving Asterisk AGI Component...{NC}\n"
+printf "${GREEN}Saving Asterisk AGI Component...${NC}\n"
 wget -O Asterisk-CallerLookup.py https://raw.githubusercontent.com/scottphilip/caller-lookup/master/Plugins/Asterisk-CallerLookup.py
 mv Asterisk-CallerLookup.py /var/lib/asterisk/agi-bin/Asterisk-CallerLookup.py
 
-printf "${GREEN}Saving Superfecta Module...{NC}\n"
+printf "${GREEN}Saving Superfecta Module...${NC}\n"
 wget -O source-CallerLookup.module https://raw.githubusercontent.com/scottphilip/caller-lookup/master/Plugins/source-CallerLookup.module.php
 mv source-CallerLookup.module /var/www/html/admin/modules/superfecta/sources/source-CallerLookup.module
 
-printf "${GREEN}Complete.{NC}\n"
+printf "${YELLOW}Complete.${NC}\n"
+
+exit
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Caller Lookup
 
-Python Wrapper for TrueCaller
+Python Wrapper for TrueCaller.  Retains Google cookies to reauthenticate quickly.
 
 ## Features:
 
@@ -10,6 +10,9 @@ Python Wrapper for TrueCaller
  * FreePBX Superfecta Source Module 
  * Asterisk AGI CName Script 
 
+## Authentication
+Requires a Google Account to authenticate against.  Supports Two step authentication as well as Android/iOS Google App Permission.
+
 ## Installers
 
 ### CentOS (FreePBX)
@@ -17,4 +20,8 @@ Python Wrapper for TrueCaller
 ```
 sudo wget -qO- https://raw.githubusercontent.com/scottphilip/caller-lookup/master/Install-CentOS.sh | bash
 ```
+
+## Author
+##### Scott Philip 
+Berlin, Germany
 
