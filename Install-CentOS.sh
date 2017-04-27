5000364a8a5bfbea8d1f4045b37c13497c3f6446
Scott Philip <sp@scottphilip.com>
2017-04-27 19:02:05 +0200

    Initial commit

--- Install-CentOS.sh
+++ Install-CentOS.sh
@@ -1,107 +1,81 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-then
-    echo "Usage: sudo wget ..."
+#Check SUDO
+if [ "$EUID" -ne 0 ]; then
+	whiptail --title 'Caller Lookup Installer' --msgbox "This must be run with SUDO or under the root account."  --ok-button 'Quit' 7 78
     exit;
 fi
 
-echo_header()
+if (!(whiptail --title 'Caller Lookup Installer' --yesno "You are about to install CallerLookup and its dependencies" --yes-button 'Continue' --no-button 'Quit' 7 62)); then
+	exit;
+fi
+
+is_python3=false
+is_pip3=false
+is_selenium=false
+is_phonenumbers=false
+
+if [ "type $python3 > /dev/null" ]; then
+
+	is_python3=true
+
+    if [ "type $pip3 > /dev/null" ]; then
+
+		is_pip3=true
+		if [ "$len( $(pip show selenium) ) -gt 0" ]; then
+			is_selenium = true
+			install_count = install_count + 1
+		fi
+		if [ [len "pip show phonenumbers"] -gt "0" ]; then
+			is_phonenumbers = true
+		fi
+	fi
+fi
+
+if ( ! $is_python3 || ! $is_pip3 || ! $is_selenium || ! $is_phonenumbers );
+then
+	installing=""
+	if (! $is_python3); then installing="$installing\n       => Python3"; fi;
+	if (! $is_pip3); then installing="$installing\n       => PIP3"; fi;
+	if (! $is_selenium); then installing="$installing\n       => Selenium"; fi;
+	if (! $is_phonenumbers); then installing="$installing\n       => PhoneNumbers"; fi;
+
+	if (!(whiptail --title 'Caller Lookup Installer' --yesno "The following dependencies will be installed with the application:\n$installing" --yes-button 'Install' --no-button 'Quit' 12 62))
+	then
+		exit
+	fi
+else
+	if (!(whiptail --title 'Caller Lookup Installer' --yesno "All dependencies are already installed." --yes-button 'Continue' --no-button 'Quit' 7 62))
+	then
+		exit
+	fi
+fi
+
 {
-    reset
-    echo "=================================================================="
-    echo "=               CallerLookup Install (CentOS)                    ="
-    echo "=================================================================="
-}
+	printf "XXX\n1\nUpdating Existing Libraries ... \nXXX"
+	unbuffer yum -y update > x
 
-install()
+	printf "XXX\n16\nAdding New Libraries ... \nXXX"
+	unbuffer yum -y install https://centos7.iuscommunity.org/ius-release.rpm > x
+
+	printf "XXX\n30\nInstalling Python 3.6 ... \nXXX"
+	unbuffer yum -y install python36u > x
+
+	printf "XXX\n60\nInstalling Python 3.6 PIP ... \nXXX"
+	unbuffer yum -y install python36u-pip > x
+
+	printf "XXX\n70\nInstalling Selenium ... \nXXX"
+	unbuffer pip install selenium > x
+
+	printf "XXX\n85\nInstalling PhoneNumbers ... \nXXX"
+	unbuffer pip install phonenumbers > x
+
+	printf "XXX\n100\nDependency Install Complete\nXXX"
+
+} | whiptail --title 'Caller Lookup Installer' --gauge "Installing ..." 6 50 0
+
 {
-    $is_python3 = $false
-    $is_pip = $false
-    $is_selenium = $false
-    $is_phonenumbers = $false
 
-    if command -v python3 > /dev/null 2>&1; then
-        $is_python3 = $true
-        if command -v pip > /dev/null 2>&1; then
-            $$is_pip = $true
-        fi
-        if [$is_pip == $true]
-        then
-            $is_selenium = "$($(len$(pip show selenium) > 0))" || $false;
-            $is_phonenumbers = "$($(len$(pip show phonenumbers) > 0))" || $false;
-        fi
-    fi
+	printf "XXX\n100\nVertification Complete\nXXX"
 
-    if [$is_python3 || $is_pip || $is_selenium || $is_phonenumbers];
-    then
-        echo_header
-        echo "The following dependencies will be installed:"
-        if [$is_python3]; then echo "       - Python3 (Yum)"; fi
-        if [is_pip]; then echo "         - PIP  (Yum)"; fi
-        if [is_selenium]; then echo "           - Selenium (PIP)"; fi
-        if [is_phonenumbers]; then echo "           - PhoneNumbers (PIP)"; fi
-        echo "Are you sure you want to install these dependencies? [Y/N]"
-
-        read $install_confirm
-        case ${install_confirm:0:1} in
-            y|Y )
-
-                echo_header
-
-                if [!$is_python3];
-                then
-                    printf "\rInstalling Python3 ... "
-                    unbuffer yum install python3 > python3_log
-                    echo "Installing Python3 ... done"
-                fi
-
-                if [!$is_pip];
-                then
-                    printf "\rInstalling PIP ... "
-                    unbuffer yum install python-pip python-wheel > pip_log
-                    unbuffer yum upgrade python-setuptools > pipsetuptools_log
-                    echo "Installing PIP ... done"
-                fi
-
-                if [!$is_selenium];
-                then
-                    printf "\rInstalling Selenium ... "
-                    unbuffer pip install selenium > selenium_log
-                    echo "Installing Selenium ... done"
-                fi
-
-                if [!$is_phonenumbers];
-                then
-                    printf "\rInstalling PhoneNumbers ... "
-                    unbuffer pip install phonenumbers > phonenumbers_log
-                    echo "Installing PhoneNumbers ... done"
-                fi
-            ;;
-            * )
-                exit
-            ;;
-        esac
-    fi
-
-    # Asterisk AGI
-    if [ -d "/var/lib/asterisk/agi-bin" ]; then
-        printf "\rInstalling Asterisk AGI Script ... "
-        unbuffer wget  -O /var/lib/asterisk/agi-bin/AgiCallerLookup.py "https://raw.githubusercontent.com/scottphilip/caller-lookup/master/Plugins/AgiCallerLookup.py" > agi_log
-        chmod +x /var/lib/asterisk/agi-bin/AgiCallerLookup.py
-        echo "Installing Asterisk AGI Script ... done"
-    fi
-
-    # FreeBPX - Superfecta
-    if [ -d "/var/www/html/admin/modules/superfecta/sources/" ]; then
-        printf "\rInstalling FreePBX Superfecta Source ... "
-        unbuffer wget  -O /var/www/html/admin/modules/superfecta/sources/source-CallerLookup.module "https://raw.githubusercontent.com/scottphilip/caller-lookup/master/Plugins/source-CallerLookup.module.php" > superfecta_log
-        echo "Installing FreePBX Superfecta Source ... done"
-    fi
-
-    echo "Install Complete"
-    exit
-
-}
-
-install
+} | whiptail --title 'Caller Lookup Installer' --gauge "Verifying ..." 6 50 0
