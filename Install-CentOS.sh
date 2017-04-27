b251cc46c7e24eed38593448caae24661e3be5fc
Scott Philip <sp@scottphilip.com>
2017-04-28 00:06:30 +0200

    Initial commit

--- Install-CentOS.sh
+++ Install-CentOS.sh
@@ -1,81 +1,49 @@
 #!/bin/bash
 
+cd /tmp
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 #Check SUDO
 if [ "$EUID" -ne 0 ]; then
-	whiptail --title 'Caller Lookup Installer' --msgbox "This must be run with SUDO or under the root account."  --ok-button 'Quit' 7 78
+	printf "${RED}This must be run as root or with sudo.{NC}\n"
     exit;
 fi
 
-if (!(whiptail --title 'Caller Lookup Installer' --yesno "You are about to install CallerLookup and its dependencies" --yes-button 'Continue' --no-button 'Quit' 7 62)); then
-	exit;
-fi
-
-is_python3=false
-is_pip3=false
-is_selenium=false
-is_phonenumbers=false
-
-if [ "type $python3 > /dev/null" ]; then
-
-	is_python3=true
-
-    if [ "type $pip3 > /dev/null" ]; then
-
-		is_pip3=true
-		if [ "$len( $(pip show selenium) ) -gt 0" ]; then
-			is_selenium = true
-			install_count = install_count + 1
-		fi
-		if [ [len "pip show phonenumbers"] -gt "0" ]; then
-			is_phonenumbers = true
-		fi
-	fi
-fi
-
-if ( ! $is_python3 || ! $is_pip3 || ! $is_selenium || ! $is_phonenumbers );
+read -r -p "Are you sure you want to install CallerLookup and its dependencies? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
-	installing=""
-	if (! $is_python3); then installing="$installing\n       => Python3"; fi;
-	if (! $is_pip3); then installing="$installing\n       => PIP3"; fi;
-	if (! $is_selenium); then installing="$installing\n       => Selenium"; fi;
-	if (! $is_phonenumbers); then installing="$installing\n       => PhoneNumbers"; fi;
-
-	if (!(whiptail --title 'Caller Lookup Installer' --yesno "The following dependencies will be installed with the application:\n$installing" --yes-button 'Install' --no-button 'Quit' 12 62))
-	then
-		exit
-	fi
+    printf "${GREEN}Starting installation...{NC}\n"
 else
-	if (!(whiptail --title 'Caller Lookup Installer' --yesno "All dependencies are already installed." --yes-button 'Continue' --no-button 'Quit' 7 62))
-	then
-		exit
-	fi
+    exit
 fi
 
-{
-	printf "XXX\n1\nUpdating Existing Libraries ... \nXXX"
-	unbuffer yum -y update > x
+printf "${GREEN}Updating Packages...{NC}\n"
+/usr/bin/yum update
 
-	printf "XXX\n16\nAdding New Libraries ... \nXXX"
-	unbuffer yum -y install https://centos7.iuscommunity.org/ius-release.rpm > x
+printf "${GREEN}Python 3.6...{NC}\n"
+/usr/bin/yum install python36u
 
-	printf "XXX\n30\nInstalling Python 3.6 ... \nXXX"
-	unbuffer yum -y install python36u > x
+printf "${GREEN}Python PIP 3.6...{NC}\n"
+/usr/bin/yum install python36u-pip
 
-	printf "XXX\n60\nInstalling Python 3.6 PIP ... \nXXX"
-	unbuffer yum -y install python36u-pip > x
+printf "${GREEN}Selenium...{NC}\n"
+/usr/bin/pip install selenium
 
-	printf "XXX\n70\nInstalling Selenium ... \nXXX"
-	unbuffer pip install selenium > x
+printf "${GREEN}PhoneNumbers...{NC}\n"
+/usr/bin/pip install phonenumbers
 
-	printf "XXX\n85\nInstalling PhoneNumbers ... \nXXX"
-	unbuffer pip install phonenumbers > x
+printf "${GREEN}Pyotp...{NC}\n"
+/usr/bin/pip install pyotp
 
-	printf "XXX\n100\nDependency Install Complete\nXXX"
+printf "${GREEN}Saving Asterisk AGI Component...{NC}\n"
+wget -O Asterisk-CallerLookup.py https://raw.githubusercontent.com/scottphilip/caller-lookup/master/Plugins/Asterisk-CallerLookup.py
+mv Asterisk-CallerLookup.py /var/lib/asterisk/agi-bin/Asterisk-CallerLookup.py
 
-} | whiptail --title 'Caller Lookup Installer' --gauge "Installing ..." 6 50 0
+printf "${GREEN}Saving Superfecta Module...{NC}\n"
+wget -O source-CallerLookup.module https://raw.githubusercontent.com/scottphilip/caller-lookup/master/Plugins/source-CallerLookup.module.php
+mv source-CallerLookup.module /var/www/html/admin/modules/superfecta/sources/source-CallerLookup.module
 
-{
-
-	printf "XXX\n100\nVertification Complete\nXXX"
-
-} | whiptail --title 'Caller Lookup Installer' --gauge "Verifying ..." 6 50 0
+printf "${GREEN}Complete.{NC}\n"
