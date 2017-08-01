d04beb7cc1a1c725030a776cbaf492dd951cf641
Scott Philip <sp@scottphilip.com>
2017-08-01 15:05:26 +0200

    1.2.2 Release

--- COPYLOGS.sh
+++ COPYLOGS.sh
@@ -6,7 +6,7 @@ LOG_REPO="test-logs"
 HOME_DIR="/home/travis"
 GIT_ROOT="/home/travis/github"
 BUILD_ARTIFACTS_ROOT="/home/travis/logs"
-REPO_PATH="git@github.com:${GITHUB_USERNAME}/${LOG_REPO}.git"
+REPO_PATH="${GITHUB_PASSWORD}@github.com/${GITHUB_USERNAME}/${LOG_REPO}.git"
 PROJECT_REPO_NAME="${TRAVIS_REPO_SLUG}"
 MESSAGE="${TRAVIS_COMMIT} (Job ${TRAVIS_JOB_NUMBER})"
 
@@ -15,26 +15,25 @@ mkdir github
 
 echo "GIT PATH: ${GIT_ROOT}"
 cd "${GIT_ROOT}"
-git config user.email ${GITHUB_EMAIL}
-git config user.name ${GITHUB_USERNAME}
-git config user.password ${GITHUB_PASSWORD}
 git init .
+git config user.email "travis@travis-ci.org"
+git config user.name "Travis CI"
 
-echo "ADDING TO GIT [${REPO_PATH}] ..."
-git clone ${REPO_PATH}
+echo "ADDING TO GIT [https://${REPO_PATH}] ..."
+git clone https://${REPO_PATH}
 
 echo "MOVING FILES..."
 mkdir -p ${GIT_ROOT}/${LOG_REPO}/${PROJECT_REPO_NAME}
 mv ${BUILD_ARTIFACTS_ROOT}/* ${GIT_ROOT}/${LOG_REPO}/${PROJECT_REPO_NAME}/*
 
 echo "ADDING FILES TO GIT..."
-git add ${GIT_ROOT}/${LOG_REPO}/${PROJECT_REPO_NAME}/${TRAVIS_JOB_NUMBER}
+git add "${GIT_ROOT}/${LOG_REPO}/${PROJECT_REPO_NAME}/${TRAVIS_JOB_NUMBER}"
 
 echo "COMMITTING CHANGES..."
 git commit -m ${MESSAGE}
 
 echo "UPLOADING FILES..."
-git remote add origin ${REPO_PATH}
+git remote add origin https://${REPO_PATH} > /dev/null 2>&1
 git push origin master
 
 echo "COMPLETE"
