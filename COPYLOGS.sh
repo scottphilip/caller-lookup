ac88bb2f2184591eecd0a9764360338b05b949c1
Scott Philip <sp@scottphilip.com>
2017-08-01 13:58:16 +0200

    1.2.2 Release

--- COPYLOGS.sh
+++ COPYLOGS.sh
@@ -3,18 +3,22 @@
 echo "SAVING LOGS..."
 ##################################################################################
 LOG_REPO="test-logs"
+HOME_DIR="/home/travis"
 GIT_ROOT="/home/travis/github/"
-BUILD_ARTIFACTS_ROOT="/home/travis/logs/"
+BUILD_ARTIFACTS_ROOT="/home/travis/logs"
 REPO_PATH="github.com/${GITHUB_USERNAME}/${LOG_REPO}.git"
 MESSAGE="${TRAVIS_COMMIT} (Job ${TRAVIS_JOB_NUMBER})"
+
+cd "${HOME_DIR}"
+mkdir github
 
 echo "GIT PATH: ${GIT_ROOT}"
 cd "${GIT_ROOT}"
 
 echo "ADDING TO GIT (https://${REPO_PATH}) ..."
 git init .
-git clone https://${REPO_PATH}
-git remote add origin https://${REPO_PATH}
+git clone https://${GITHUB_PASSWORD}@${REPO_PATH}
+git remote add origin https://${GITHUB_PASSWORD}@${REPO_PATH}
 
 mv ${BUILD_ARTIFACTS_ROOT}/* ${GIT_ROOT}/${LOG_REPO}/*
 git add *
