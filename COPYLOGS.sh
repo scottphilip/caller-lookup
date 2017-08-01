8154ccf6a3e0559b135ff3f96dff7f70a1dd1684
Scott Philip <sp@scottphilip.com>
2017-08-01 16:35:58 +0200

    1.2.2 Release

--- COPYLOGS.sh
+++ COPYLOGS.sh
@@ -7,7 +7,7 @@ PROJECT_REPO_NAME="caller-lookup"
 JOB_DIR="/home/travis/logs"
 GIT_DIR="/home/travis/github"
 REPO_PATH="${GITHUB_PASSWORD}@github.com/${GITHUB_USERNAME}/${LOG_REPO}.git"
-MESSAGE="${TRAVIS_COMMIT} (Job ${TRAVIS_JOB_NUMBER})"
+MESSAGE="${TRAVIS_JOB_NUMBER}"
 LOG_JOB_PATH="${JOB_DIR}/${TRAVIS_JOB_NUMBER}"
 GIT_JOB_PATH="${GIT_DIR}/${LOG_REPO}/${PROJECT_REPO_NAME}/${TRAVIS_JOB_NUMBER}"
 
@@ -32,7 +32,7 @@ echo "ADDING FILES TO GIT... [${GIT_JOB_PATH}]"
 git add "${GIT_JOB_PATH}"/*
 
 echo "COMMITTING CHANGES... [${MESSAGE}]"
-git commit -m ${MESSAGE}
+git commit -m "${MESSAGE}"
 
 echo "UPLOADING FILES..."
 git remote add origin https://${REPO_PATH} > /dev/null 2>&1
